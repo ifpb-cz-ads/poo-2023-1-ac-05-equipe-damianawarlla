3b554b285479f35f2622fc7d3ce7baf7e520562a
Warlla0101 <104159596+Warlla0101@users.noreply.github.com>
2023-04-12 17:21:37 -0300

    Add files via upload

---
+++ CBBQuestao5.java
@@ -0,0 +1,16 @@
+public class CBBQuestao5 {
+
+    public static void main(String[] args) {
+        
+        int numero = 0;
+
+        while (numero <=100){
+            if (numero%2 == 0) {
+                System.out.println(numero);
+            }
+            numero++;
+        }
+
+    }
+
+}
