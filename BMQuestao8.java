f489cb5e478fac0e16a3202b061ca2e29b8b1899
Warlla0101 <104159596+Warlla0101@users.noreply.github.com>
2023-04-12 17:22:36 -0300

    Add files via upload

---
+++ BMQuestao8.java
@@ -0,0 +1,14 @@
+public class BMQuestao8 {
+    public static void main(String[] args) {
+        
+        int numero = 0;
+
+        while (numero <=100){
+            if (numero%3 == 0) {
+                System.out.println(numero);
+            }
+            numero++;
+        }
+
+    }
+}
